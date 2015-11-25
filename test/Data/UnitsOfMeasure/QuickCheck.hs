7520ca08bffe97aa919af98d9bde4e56f955e412
Joe Hermaszewski <git@monoid.al>
2015-11-25 14:45:50 +0000

    Derive random for quantities

--- test/Data/UnitsOfMeasure/QuickCheck.hs
+++ test/Data/UnitsOfMeasure/QuickCheck.hs
@@ -7,12 +7,15 @@ module Data.UnitsOfMeasure.QuickCheck
   ( PositiveQuantity(..)
   ) where
 
-import Test.QuickCheck (Arbitrary(..), Positive(..))
 import Data.UnitsOfMeasure.Internal (Quantity(..))
+import System.Random (Random)
+import Test.QuickCheck (Arbitrary(..), Positive(..))
 
 newtype PositiveQuantity a = PositiveQuantity { getPositiveQuantity :: a }
 
 deriving instance Arbitrary a => Arbitrary (Quantity a u)
+
+deriving instance Random a => Random (Quantity a u)
 
 instance (Num a, Ord a, Arbitrary a) => Arbitrary (PositiveQuantity (Quantity a u)) where
   arbitrary = PositiveQuantity . MkQuantity . getPositive <$> arbitrary
