2b5910cf8ae9d102fec05a47933d48144db8093c
Joe Hermaszewski <git@monoid.al>
2015-11-25 15:14:29 +0000

    Reorganize tests

--- test/Test.hs
+++ test/Test.hs
@@ -5,32 +5,35 @@ module Main
 
 import Physics.Orbit
 import Physics.Orbit.QuickCheck
-import Test.Tasty.QuickCheck (testProperty, Property, (===))
+import Test.Tasty (testGroup, TestTree)
+import Test.Tasty.QuickCheck (testProperty, (===))
 import Test.Tasty.TH (defaultMainGenerator)
 
-prop_validCircularOrbit :: CircularOrbit Double -> Bool
-prop_validCircularOrbit (CircularOrbit o) = isValid o
+test_sanity :: [TestTree]
+test_sanity = [ testProperty "circular isValid"
+                  (\(CircularOrbit o) -> isValid (o :: Orbit Double))
+              , testProperty "elliptic isValid"
+                  (\(EllipticOrbit o) -> isValid (o :: Orbit Double))
+              , testProperty "parabolic isValid"
+                  (\(ParabolicOrbit o) -> isValid (o :: Orbit Double))
+              , testProperty "hyperbolic isValid"
+                  (\(HyperbolicOrbit o) -> isValid (o :: Orbit Double))
+              ]
 
-prop_validEllipticOrbit :: EllipticOrbit Double -> Bool
-prop_validEllipticOrbit (EllipticOrbit o) = isValid o
-
-prop_validParabolicOrbit :: ParabolicOrbit Double -> Bool
-prop_validParabolicOrbit (ParabolicOrbit o) = isValid o
-
-prop_validHyperbolicOrbit :: HyperbolicOrbit Double -> Bool
-prop_validHyperbolicOrbit (HyperbolicOrbit o) = isValid o
-
-prop_classifyCircularOrbit :: CircularOrbit Double -> Property
-prop_classifyCircularOrbit (CircularOrbit o) = classify o === Elliptic
-
-prop_classifyEllipticOrbit :: EllipticOrbit Double -> Property
-prop_classifyEllipticOrbit (EllipticOrbit o) = classify o === Elliptic
-
-prop_classifyParabolicOrbit :: ParabolicOrbit Double -> Property
-prop_classifyParabolicOrbit (ParabolicOrbit o) = classify o === Parabolic
-
-prop_classifyHyperbolicOrbit :: HyperbolicOrbit Double -> Property
-prop_classifyHyperbolicOrbit (HyperbolicOrbit o) = classify o === Hyperbolic
+test_classify :: [TestTree]
+test_classify = [ testProperty "circular"
+                    (\(CircularOrbit o) ->
+                       classify (o :: Orbit Double) === Elliptic)
+                , testProperty "elliptic"
+                    (\(EllipticOrbit o) ->
+                       classify (o :: Orbit Double) === Elliptic)
+                , testProperty "parabolic"
+                    (\(ParabolicOrbit o) ->
+                       classify (o :: Orbit Double) === Parabolic)
+                , testProperty "hyperbolic"
+                    (\(HyperbolicOrbit o) ->
+                       classify (o :: Orbit Double) === Hyperbolic)
+                ]
 
 
 main :: IO ()
