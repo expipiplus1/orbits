2a1ac3165cc968e98593b186fe117eb245135b14
Joe Hermaszewski <git@monoid.al>
2020-09-29 15:03:35 +0800

    Add some more planets to sol

--- src/Physics/Orbit/Sol.hs
+++ src/Physics/Orbit/Sol.hs
@@ -13,6 +13,18 @@ solMass = 1988500e24 % [si|kg|]
 solGraviationalParameter :: Fractional a => Quantity [si| m^3 s^-2 |] a
 solGraviationalParameter = solMass |*| gravity_G
 
+venusOrbit :: Fractional a => Orbit a
+venusOrbit = Orbit
+  { eccentricity                  = 0.006772
+  , periapsis                     = 0.718440 % AstronomicalUnit
+  , inclinationSpecifier          = Inclined
+                                      { longitudeOfAscendingNode = 76.680 % [si|deg|]
+                                      , inclination = 2.19 % [si|deg|]
+                                      }
+  , periapsisSpecifier = Eccentric { argumentOfPeriapsis = 54.884 % [si|deg|] }
+  , primaryGravitationalParameter = solGraviationalParameter
+  }
+
 earthOrbit :: Fractional a => Orbit a
 earthOrbit = Orbit
   { eccentricity                  = 0.01671123
@@ -21,9 +33,36 @@ earthOrbit = Orbit
                                       { longitudeOfAscendingNode = 348.73936 % [si|deg|]
                                       , inclination = 1.578690 % [si|deg|]
                                       }
-  , periapsisSpecifier = Eccentric { argumentOfPeriapsis = 114.20783 % [si|deg|] }
+  , periapsisSpecifier = Eccentric { argumentOfPeriapsis = 114.20783 % [si|deg|]
+                                   }
   , primaryGravitationalParameter = solGraviationalParameter
   }
+
+marsOrbit :: Fractional a => Orbit a
+marsOrbit = Orbit
+  { eccentricity                  = 0.0934
+  , periapsis                     = 1.382 % AstronomicalUnit
+  , inclinationSpecifier          = Inclined
+                                      { longitudeOfAscendingNode = 49.558 % [si|deg|]
+                                      , inclination = 1.67 % [si|deg|]
+                                      }
+  , periapsisSpecifier = Eccentric { argumentOfPeriapsis = 286.502 % [si|deg|] }
+  , primaryGravitationalParameter = solGraviationalParameter
+  }
+
+halleyOrbit :: Fractional a => Orbit a
+halleyOrbit = Orbit
+  { eccentricity                  = 0.96714
+  , periapsis                     = 0.586 % AstronomicalUnit
+  , inclinationSpecifier          = Inclined
+                                      { longitudeOfAscendingNode = 58.42 % [si|deg|]
+                                      , inclination = 162.26 % [si|deg|]
+                                      }
+  , periapsisSpecifier = Eccentric { argumentOfPeriapsis = 111.33 % [si|deg|] }
+  , primaryGravitationalParameter = solGraviationalParameter
+  }
+
+
 
 -- | The fastest comet in the west. Nice for testing as it's on a hyperbolic
 -- trajectory. See https://en.wikipedia.org/wiki/C/1980_E1
